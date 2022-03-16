8ec7d9b0f37bb6a8ee1fd44128966161364ea68a
Filippo Gambarota <filippo.gambarota@gmail.com>
2022-03-16 11:09:23 +0100

    adding auto commit and push

---
+++ test/upload_data.py
@@ -0,0 +1,36 @@
+"""
+Auto commit data in Python
+"""
+
+import subprocess
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# pulling remote
+subprocess.call(["git", "pull"])
+ 
+# subject number
+subj_number = input("Inserisci il numero del soggetto: ")
+
+# adding all relevant files
+subprocess.call(["git", "add", "data/"])
+
+# commit message
+msg = "adding subject {}".format(subj_number)
+
+# commit
+subprocess.call(["git", "commit", "-m", msg])
+
+# pushing to remote
+subprocess.call(["git", "push"])
+
+print(bcolors.OKGREEN + "I dati sono stati salvati! :)" + bcolors.OKGREEN)
