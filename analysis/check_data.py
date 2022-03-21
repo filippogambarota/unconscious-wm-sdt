b9e9d4fd89a69e3e1757afdba0181bc9a6cd21b1
Filippo Gambarota <filippo.gambarota@gmail.com>
2022-03-21 20:46:11 +0100

    cleaning repo

--- analysis/check_data.py
+++
@@ -1,9 +0,0 @@
-from fileinput import filename
-import pickle
-import os
-
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, '../data/session/s45_session.pkl')
-
-with open(filename, "rb") as file:
-    backup = pickle.load(file)
